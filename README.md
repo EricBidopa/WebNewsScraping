79908ccd02188a273f1166ee167a7ef0f2ac96d1
EricAddoJr <ericaddojunior100@gmail.com>
2023-04-12 11:50:39 +0000

    Scraping python script Done :)

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # WebNewsScraping
 This script scraps latest news on ycombinator's hacker news website and sends them to your email
+
+# Definitions
+
+Python script that scrapes top news stories from the Hacker News website and sends an email 
+containing the scraped content to a specified email address.
+
+## The code makes extensive use of the following libraries:
+
+requests:
+for making HTTP requests to the website
+BeautifulSoup:
+for parsing the HTML content of the website and extracting the news stories
+smtplib:
+for sending the email
+email.mime:
+for creating the email message body
+
+The code defines a function extractNews which takes a URL as input, sends a 
+GET request to the URL, and then uses BeautifulSoup to extract the top
+ news stories from the HTML content of the website. The extracted news stories are returned as a string.
+
+ The code then calls the extractNews function to scrape the top news stories 
+ from Hacker News and adds the scraped content to a string variable content.
+
+ Next, the code creates an email message body by attaching the content 
+ string to an instance of MIMEMultipart. The code also sets the email subject, 
+ from address, and to address.
+
+ The code then sets up an SMTP server to send the email using smtplib, 
+ logs in to the from email address using the provided password, sends the email message, 
+ and finally quits the server.
+
+Note that some variables in the code (such as the from email address, password, and to email address)
+should be updated to match your email credentials. 
+
+
+# Getting Started
+1. Install all library listed above. most of them comes with python installation by default.
+2. clone this repository 'git clone ......'. replace '..........' with the SSH link.
+3. navigate the code to locate the input section of your email credentials (email address and your server's SMTP port, etc.)
+4. input into the password section(FROMPASS) the password of the email account that you specified.If that email account has 
+2Fa enables, you will have to disable it in your google account or generate 'app password' from google by searching 'app password'
+in your search tab in your google account security page :)
+ 
+
+
+# Cheers!
+
+
+
+
+
+
+
+
+
+
---
+++ webScrapping.py
@@ -0,0 +1,94 @@
+import requests  # for the http requests
+
+from bs4 import BeautifulSoup  # for web scraping
+
+import smtplib  # for email autentication and transfers
+
+from email.mime.multipart import MIMEMultipart  # used for creating the email body
+from email.mime.text import MIMEText  # for the email body text.
+import datetime
+
+currentTime = datetime.datetime.now()
+
+content = ""
+
+
+# extracting the News Stories
+
+
+def extractNews(url):
+    print("Extracting News Stories....")
+    blockcnt = ""
+    blockcnt += "<b>HN Top Stories:</b>\n" + "<br>" + "-" * 50 + "<br>"
+    response = requests.get(url)
+    blockContent = response.content
+    soup = BeautifulSoup(blockContent, "html.parser")
+    for i, tag in enumerate(
+        soup.find_all("td", attrs={"class": "title", "valign": ""})
+    ):
+        blockcnt += (
+            (
+                str(i + 1)
+                + " :: "
+                + '<a href="'
+                + tag.a.get("href")
+                + '">'
+                + tag.text
+                + "</a>"
+                + "\n"
+                + "<br>"
+            )
+            if tag.text != "More"
+            else ""
+        )
+    return blockcnt
+
+
+cnt = extractNews("https://news.ycombinator.com/")
+content += cnt
+content += "<br>----Email Finished----<br>"
+
+# Sending the email.
+print("Composing Email..Hold on :)....")
+
+# update your email details
+# make sure to update the Google Low App Access settings before
+
+SERVER = "smtp.gmail.com"  # "The email server-base". smtp server for gmail
+PORT = 587  # port number for gmail
+FROM = "**ericaddojr@gmail.com"  # "from email address" ENTER
+TO = "ericaddojr@gmail.com"  #  to email address(es)  ENTER
+FROMPASS = "******"  # Password of *From* address above. ENTER. If 2Fa is on your google account, you need to generate an app access password from myaccount.google.com/security by searching 'app password' in the search bar. :)
+
+# Creating message body
+msg = MIMEMultipart()
+
+msg[
+    "Subject"
+] = (  # Email subject below. Concatenating with date and time to create a unique subject every time to escape gmail's torture :)
+    "Top News Stories HN [Automated Email]"
+    + " "
+    + str(currentTime.day)
+    + "-"
+    + str(currentTime.month)
+    + "-"
+    + str(currentTime.year)
+)
+msg["From"] = FROM
+msg["To"] = TO
+
+msg.attach(MIMEText(content, "html"))
+
+print("Initiating Server...")
+
+server = smtplib.SMTP(SERVER, PORT)  # SERVER and PORT above
+server.set_debuglevel(1)  # want to see error messages
+server.ehlo()
+server.starttls()
+# server.ehlo
+server.login(FROM, FROMPASS)  # login into the FROM email.
+server.sendmail(FROM, TO, msg.as_string())  # Send the email.
+
+print("Email Sent...")
+
+server.quit()  # Quit from the server afterwards.
